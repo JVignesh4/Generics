07f3f4ef62d074460c9c71ae7ab0c818d476120b
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-29 11:15:05 +0530

    Added: To Find Maximum Of Given Three Integers

--- com/blz/generics/Main.java
+++ com/blz/generics/Main.java
@@ -2,5 +2,18 @@ package com.blz.generics;
 
 public class Main {
     public static void main(String[] args) {
+        Integer max = testMaximumNumber(9, 3, 6);
+        System.out.println("maximum number is: " + max);
+    }
+
+
+    private static Integer testMaximumNumber(Integer first, Integer second, Integer third) {
+        Integer max = first;
+        if(second.compareTo(max)>0)
+            max=second;
+        if(third.compareTo(max)>0)
+            max=third;
+        return max;
+    }
     }
 }
