36f5b639d0dcbec3b4ad4de1d09244ae6522b329
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-29 11:28:18 +0530

    Added: To Find Maximum On String

--- com/blz/generics/Main.java
+++ com/blz/generics/Main.java
@@ -4,12 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Integer maxInt=testMaximumNumber(9, 3, 6);
         Float maxFloat=testMaximumNumber(9.4f, 3.5f, 9.9f);
+        String maxString= testMaximum("Apple"  , "Peach", "Banana");
         System.out.println("maximum integer is: "+maxInt);
         System.out.println("maximum float is: "+maxFloat);
+        System.out.println("maximum String is: "+maxString);
     }
 
     private static Integer testMaximumNumber(Integer first, Integer second, Integer third) {
         Integer max = first;
+        if(second.compareTo(max)>0)
+            max=second;
+        if(third.compareTo(max)>0)
+            max=third;
+        return max;
+    }
+
+    private static String testMaximum(String first, String second, String third) {
+        String max = first;
         if(second.compareTo(max)>0)
             max=second;
         if(third.compareTo(max)>0)
